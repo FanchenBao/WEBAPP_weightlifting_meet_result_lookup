bc09b6c6ae599ae0e48d4ab79d0e87aba1d8d1ae
Fanchen Bao <jonathonbao@gmail.com>
2019-07-28 20:09:31 -0400

    Add postgres config and setup database in app.py

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ Following the tutorial from 'https://realpython.com/flask-by-example-part-1-proj
 """
 import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 @app.route('/')
