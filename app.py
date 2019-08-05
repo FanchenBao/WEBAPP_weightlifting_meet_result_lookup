a2464ed55e25caa728c233f485c651d2ffb7655e
Fanchen Bao <jonathonbao@gmail.com>
2019-08-04 20:33:56 -0400

    Complete function to import all current lifting data

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def import_weightlifting_data(base_url, start_url, start_year, end_year):
     """
     ws = WebScrape(base_url, start_url)
     for year in range(start_year, end_year + 1):
-        event_urls = ws.scrape_year(year)
+        event_urls = ws.scrape_year(year)  # get all urls for events
         for event_url in event_urls:
             m = re.search(r'event=(\d+)', event_url)
             event_id = m.group(1)  # get event_id. It is used to identify whether an event has been recorded already
@@ -51,11 +51,20 @@ def import_all():
     base_url = 'https://www.iwf.net'
     old_bw_start_url = '/results/results-by-events/?event_year='  # duration 1998 - 2018
     new_bw_start_url = '/new_bw/results_by_events/?event_year='  # duration 2018 - present
-    # import old body weight
-    import_weightlifting_data(base_url, old_bw_start_url, 1998, 2018)
-    # import new body weight
-    import_weightlifting_data(base_url, new_bw_start_url, 2018, 2019)
 
+    table_exists = Result.query.limit(1).all()
+    if table_exists:  # check whether the database has already been populated
+        error = "ERROOR: Database already populated."
+    else:
+        try:
+            # import old body weight
+            import_weightlifting_data(base_url, old_bw_start_url, 1998, 2018)
+            # import new body weight
+            import_weightlifting_data(base_url, new_bw_start_url, 2018, 2019)
+            error = "Success"
+        except:
+            error = 'ERROR: Import failed'
+    return render_template('import_outcome.html', error=error)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -3,6 +3,9 @@
 Following the tutorial from 'https://realpython.com/flask-by-example-part-2-postgres-sqlalchemy-and-alembic/'
 """
 from app import db
+from datetime import datetime
+
+datetime_object = datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
 
 
 class Result(db.Model):
@@ -33,26 +36,27 @@ class Result(db.Model):
 
     def __init__(self, athlete, event_id):
         self.event_id = event_id
-        self.gender = athlete.gender
-        self.rank = athlete.rank
-        self.rank_s = athlete.rank_s
-        self.rank_cj = athlete.rank_cj
-        self.name = athlete.name
-        self.born = athlete.born
-        self.nation = athlete.nation
-        self.category = athlete.category
-        self.bweight = athlete.bweight
-        self.snatch1 = athlete.snatch1
-        self.snatch2 = athlete.snatch2
-        self.snatch3 = athlete.snatch3
-        self.snatch = athlete.snatch
-        self.jerk1 = athlete.jerk1
-        self.jerk2 = athlete.jerk2
-        self.jerk3 = athlete.jerk3
-        self.jerk = athlete.jerk
-        self.total = athlete.total
-        self.event = athlete.event
-        self.date = athlete.date
+        self.gender = athlete['gender']
+        # Use 1000 as a marker for bombed-out ranking to avoid the '---' marker in the original data set
+        self.rank = athlete['rank'] if athlete['rank'].isdigit() else 1000
+        self.rank_s = athlete['rank_s'] if athlete['rank_s'].isdigit() else 1000
+        self.rank_cj = athlete['rank_cj'] if athlete['rank_cj'].isdigit() else 1000
+        self.name = athlete['name']
+        self.born = datetime.strptime(athlete['born'], '%d.%m.%Y')
+        self.nation = athlete['nation']
+        self.category = athlete['category']
+        self.bweight = athlete['bweight']
+        self.snatch1 = athlete['snatch1']
+        self.snatch2 = athlete['snatch2']
+        self.snatch3 = athlete['snatch3']
+        self.snatch = athlete['snatch']
+        self.jerk1 = athlete['jerk1']
+        self.jerk2 = athlete['jerk2']
+        self.jerk3 = athlete['jerk3']
+        self.jerk = athlete['jerk']
+        self.total = athlete['total']
+        self.event = athlete['event']
+        self.date = datetime.strptime(athlete['date'], '%m.%Y')
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
