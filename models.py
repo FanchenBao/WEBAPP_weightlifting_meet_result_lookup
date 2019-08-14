67cbadcc62b317f845f8b5843dda326ce6aab98e
Fanchen Bao <jonathonbao@gmail.com>
2019-08-14 14:38:39 -0400

    Add strip() to each string value of  raw data

--- models.py
+++ models.py
@@ -36,27 +36,27 @@ class Result(db.Model):
 
     def __init__(self, athlete, event_id):
         self.event_id = event_id
-        self.gender = athlete['gender'].lower()
+        self.gender = athlete['gender'].lower().strip()  # unifying the representation of gender
         # Use 1000 as a marker for bombed-out ranking to avoid the '---' marker in the original data set
-        self.rank = athlete['rank'] if athlete['rank'].isdigit() else 1000
-        self.rank_s = athlete['rank_s'] if athlete['rank_s'].isdigit() else 1000
-        self.rank_cj = athlete['rank_cj'] if athlete['rank_cj'].isdigit() else 1000
-        self.name = athlete['name']
-        self.born = datetime.strptime(athlete['born'], '%d.%m.%Y')
-        self.nation = athlete['nation'].upper()
-        self.weight_class = athlete['category']
-        self.bweight = athlete['bweight']
-        self.snatch1 = athlete['snatch1']
-        self.snatch2 = athlete['snatch2']
-        self.snatch3 = athlete['snatch3']
-        self.snatch = athlete['snatch']
-        self.jerk1 = athlete['jerk1']
-        self.jerk2 = athlete['jerk2']
-        self.jerk3 = athlete['jerk3']
-        self.jerk = athlete['jerk']
-        self.total = athlete['total']
-        self.meet = athlete['event']
-        self.date = datetime.strptime(athlete['date'], '%m.%Y')
+        self.rank = athlete['rank'].strip() if athlete['rank'].strip().isdigit() else 1000
+        self.rank_s = athlete['rank_s'].strip() if athlete['rank_s'].strip().isdigit() else 1000
+        self.rank_cj = athlete['rank_cj'].strip() if athlete['rank_cj'].strip().isdigit() else 1000
+        self.name = athlete['name'].strip()
+        self.born = datetime.strptime(athlete['born'].strip(), '%d.%m.%Y')
+        self.nation = athlete['nation'].upper().strip()  # unifying the representation of nation
+        self.weight_class = athlete['category'].strip()
+        self.bweight = athlete['bweight'].strip()
+        self.snatch1 = athlete['snatch1'].strip()
+        self.snatch2 = athlete['snatch2'].strip()
+        self.snatch3 = athlete['snatch3'].strip()
+        self.snatch = athlete['snatch'].strip()
+        self.jerk1 = athlete['jerk1'].strip()
+        self.jerk2 = athlete['jerk2'].strip()
+        self.jerk3 = athlete['jerk3'].strip()
+        self.jerk = athlete['jerk'].strip()
+        self.total = athlete['total'].strip()
+        self.meet = athlete['event'].strip()
+        self.date = datetime.strptime(athlete['date'].strip(), '%m.%Y')
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
